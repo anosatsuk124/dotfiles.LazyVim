2f9136124f25fbb4fb2d3449e569fff71b0a4b34
Satsuki Akiba <anosatsuk124@gmail.com>
2025-04-01 10:47:50 +0900

    feat: add dedicated markdown language support

---
+++ lua/plugins/lang-markdown.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      anti_conceal = {
+        enabled = true,
+        -- Which elements to always show, ignoring anti conceal behavior. Values can either be booleans
+        -- to fix the behavior or string lists representing modes where anti conceal behavior will be
+        -- ignored. Possible keys are:
+        --  head_icon, head_background, head_border, code_language, code_background, code_border
+        --  dash, bullet, check_icon, check_scope, quote, table_border, callout, link, sign
+        above = 1,
+        below = 1,
+      },
+      win_options = {
+        conceallevel = {
+          default = vim.api.nvim_get_option_value("conceallevel", {}),
+          rendered = 0,
+        },
+      },
+    },
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+  },
+}
