748ad12c059000d7ff14654d47e288ad09bc396f
Satsuki Akiba <anosatsuk124@gmail.com>
2025-05-18 05:31:42 +0900

    Update

--- lua/plugins/lang-rust.lua
+++ lua/plugins/lang-rust.lua
@@ -1,3 +1,18 @@
 return {
   { import = "lazyvim.plugins.extras.lang.rust" },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        rust_analyzer = {
+          diagnostics = {
+            enable = true,
+            disabled = { "unresolved-proc-macro" },
+            enableExperimental = true,
+          },
+        },
+      },
+    },
+  },
 }
