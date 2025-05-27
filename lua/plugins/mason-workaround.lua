6c5adf9025621086fd1d451df01cd09d1b77f2e2
Satsuki Akiba <anosatsuk124@gmail.com>
2025-05-27 10:26:52 +0900

    Update

---
+++ lua/plugins/mason-workaround.lua
@@ -0,0 +1,5 @@
+-- NOTE: This is a workaround for the issue with mason.nvim not being able to
+return {
+  { "mason-org/mason.nvim", version = "^1.0.0" },
+  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
+}
