a0803b422c28183c8e32b2bc344149740a659112
Satsuki Akiba <anosatsuk124@gmail.com>
2025-06-27 11:53:10 +0900

    Replace theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,12 +1,22 @@
 return {
   {
-    "Mofiqul/dracula.nvim",
+    "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
-    config = function(_, _)
-      vim.cmd([[colorscheme dracula]])
+    opts = {},
+    config = function(_, opts)
+      require("tokyonight").setup(opts)
+      vim.cmd([[colorscheme tokyonight-storm]])
     end,
   },
+  --  {
+  --    "Mofiqul/dracula.nvim",
+  --    lazy = false,
+  --    priority = 1000,
+  --    config = function(_, _)
+  --      vim.cmd([[colorscheme dracula]])
+  --    end,
+  --  },
   {
     "nvim-lualine/lualine.nvim",
     opts = function(_, opts)
